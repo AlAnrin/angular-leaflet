e92458f929d4ac1e19fa0da2d79e03ff22b34213
SIENA\lian <lianirvi@gmail.com>
2021-01-24 15:26:00 +0300

    some tests (hiddent group, add point, delete point)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,16 +33,18 @@ describe('AppComponent', () => {
   });
 
   it('hidden group works', () => {
+    const hiddenIndex = 0;
     fixture.detectChanges();
     let titles = fixture.debugElement.queryAll(By.css('.title'));
     expect(titles.length).toBe(app.groups.map(x => x.array.length)
       .reduce((acc, item) => acc + item));
 
     const hiddenButtons = fixture.debugElement.queryAll(By.css('.hidden-list-icon'));
-    hiddenButtons[0].nativeElement.click();
+    hiddenButtons[hiddenIndex].nativeElement.click();
     fixture.detectChanges();
     titles = fixture.debugElement.queryAll(By.css('.title'));
-    expect(titles.length).toBe(app.groups.map((x, index) => index != 0 ? x.array.length : 0)
+    expect(titles.length).toBe(app.groups.map((x, index) =>
+      index != hiddenIndex ? x.array.length : 0)
       .reduce((acc, item) => acc + item));
   });
 
@@ -52,5 +54,42 @@ describe('AppComponent', () => {
     titles[0].nativeElement.click();
     fixture.detectChanges();
     expect(titles[0].classes.active).toBe(true);
+  });
+
+  it('delete marker', () => {
+    fixture.detectChanges();
+    const oldTitles = fixture.debugElement.queryAll(By.css('.title'));
+    const deleteIcons = fixture.debugElement.queryAll(By.css('.delete-icon-button'));
+    deleteIcons[0].nativeElement.click();
+    fixture.detectChanges();
+    const dialogs = fixture.debugElement.queryAll(By.css(`.app-dialog-component`));
+    const deleteButton = dialogs[1].query(By.css('.submit-button'));
+    deleteButton.nativeElement.click();
+    fixture.detectChanges();
+    const newTitles = fixture.debugElement.queryAll(By.css('.title'));
+    expect(oldTitles.length - 1).toBe(newTitles.length);
+  });
+
+  it('add marker', () => {
+    fixture.detectChanges();
+    const oldTitles = fixture.debugElement.queryAll(By.css('.title'));
+    const map = fixture.debugElement.query(By.css('#map'));
+    map.nativeElement.click();
+    fixture.detectChanges();
+    const dialogs = fixture.debugElement.queryAll(By.css(`.app-dialog-component`));
+    const addDialog = dialogs[0];
+    const nameInput = addDialog.query(By.css('#name')).nativeElement;
+    nameInput.value = 'test point';
+    nameInput.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+    app.selectGroupForCreate = app.groups[1];
+    const groupSelect = addDialog.query(By.css('#groups')).nativeElement;
+    groupSelect.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+    const addButton = addDialog.query(By.css('.submit-button'));
+    addButton.nativeElement.click();
+    fixture.detectChanges();
+    const newTitles = fixture.debugElement.queryAll(By.css('.title'));
+    expect(oldTitles.length + 1).toBe(newTitles.length);
   });
 });
--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import {FormsModule} from '@angular/forms';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -8,6 +9,9 @@ describe('ListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [ ListComponent ]
     })
     .compileComponents();
